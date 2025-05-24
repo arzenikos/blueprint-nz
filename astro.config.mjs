fa9b2209bfd8ef396966ead23f3e936156d3adcb
arsenie-sarmiento <cpt.iqhwa.vx7@gmail.com>
2025-05-24 14:33:45 +1200

    linked to netlify and astro icon integrated

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,13 @@ import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
+import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()]
     },
-    integrations: [react()],
+    integrations: [react(), icon()],
     devToolbar: {
         enabled: false
     },
