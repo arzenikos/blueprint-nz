25d76e02bfb4622776e313f94393eb0ba5f94b2f
arsenie-sarmiento <cpt.iqhwa.vx7@gmail.com>
2025-06-14 23:23:07 +1200

    customised user tabs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,19 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+// import { viteStaticCopy } from 'vite-plugin-static-copy';
 import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
     vite: {
         plugins: [
             tailwindcss(),
-            viteStaticCopy({
-                targets: [{
-                    src:'./src/**/*', // copy all files under src
-                    dest: './' // place at root of dist/
-                }]
-            })
+            // viteStaticCopy({
+            //     targets: [{
+            //         src:'./src/**/*', // copy all files under src
+            //         dest: './' // place at root of dist/
+            //     }]
+            // })
         ]
     },
     integrations: [react(), icon()],
