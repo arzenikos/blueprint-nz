05b4295983d6ffc7475f7cb7f4436f1b7182aeef
arsenie-sarmiento <cpt.iqhwa.vx7@gmail.com>
2025-06-14 13:20:12 +1200

    refactored home page

--- src/pages/blobs/_components/StoredShapes.tsx
+++ src/pages/blobs/_components/StoredShapes.tsx
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 // <!-- Imported by: pages/blobs/_components/ShapeEditor.tsx -->
-=======
 // <!-- Referenced by: pages/blobs/_components/ShapeEditor.tsx -->
->>>>>>> feature/iteration-002-gamified-bluenode
 import { useState, useEffect } from 'react';
 import ShapePreview from './ShapePreview.tsx';
 import { generateBlob } from '../../../lib/utils/helpers';
