041d5865914be22fe3c50969bc6eb4b8c01e06a5
arsenie-sarmiento <cpt.iqhwa.vx7@gmail.com>
2025-06-18 05:53:16 +1200

    adjusted responsive vertical layout

--- src/content/home-user-tab.ts
+++ src/content/home-user-tab.ts
@@ -1,33 +1,13 @@
 export const userContent = {
-    patient: [
-        {
-        type: 'Patient',
-        icon: '0',
-        label: 'patient0'
-        },
-        {
-        type: 'Patient',
-        icon: '1',
-        label: 'patient1'
-        },
-        {
-        type: 'Patient',
-        icon: '2',
-        label: 'patient2'
-        },
-        {
-        type: 'Patient',
-        icon: '3',
-        label: 'patient3'
-        },              
-        {
-        type: 'Patient',
-        icon: '4',
-        label: 'patient4'
-        },         
+    users: [
+        { icon: '0', label: 'patient0', type: 'Patient'},
+        { icon: '1', label: 'patient1', type: 'Patient'},
+        { icon: '2', label: 'patient2', type: 'Patient'},
+        { icon: '3', label: 'patient3', type: 'Patient'},              
+        { icon: '4', label: 'patient4', type: 'Patient'},         
     ],
     other: [
-        { label: 'Caregiver', icon: 'caregiver', type:  'Others' },
-        { label: 'Researcher', icon: 'researcher', type:  'Others' },
+        { icon: 'caregiver', label: 'Caregiver', type:  'Caregiver' },
+        { icon: 'researcher', label: 'Researcher', type:  'Researcher' },
     ],
 }
