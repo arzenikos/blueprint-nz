3a96b2eebadc9a5c760b4a616d24f2ac98e2fb8e
arsenie-sarmiento <cpt.iqhwa.vx7@gmail.com>
2025-06-18 05:24:47 +1200

    adjusted responsive vertical layout

--- src/content/home-hero.ts
+++ src/content/home-hero.ts
@@ -18,39 +18,4 @@ export const heroContent = {
         alt: "Hero image 3"
       }
     ],
-  // infoSection: {
-  //   title: "Information & Support",
-  //   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  //   ctaText: "Read more",
-  //   ctaLink: "#"
-  // },
-  // servicesSection: {
-  //   title: "Our Services",
-  //   services: [
-  //     {
-  //       title: "Get Involved",
-  //       image: "placeholder-image.jpg",
-  //       alt: "Get Involved",
-  //       link: "/get-involved"
-  //     },
-  //     {
-  //       title: "Research",
-  //       image: "placeholder-image.jpg",
-  //       alt: "Research",
-  //       link: "/research"
-  //     },
-  //     {
-  //       title: "Shop",
-  //       image: "placeholder-image.jpg",
-  //       alt: "Shop",
-  //       link: "/shop"
-  //     }
-  //   ]
-  // },
-  // foundationSection: {
-  //   title: "Prostate Cancer Foundation NZ",
-  //   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  //   ctaText: "Read more",
-  //   ctaLink: "/prostate-cancer-foundation"
-  // }
 };
