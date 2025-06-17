2ebf0b4c826b972bbd816f6240c27783d13d3071
arsenie-sarmiento <cpt.iqhwa.vx7@gmail.com>
2025-06-18 11:35:46 +1200

    resolved conflict on branch001

--- src/content/home-user-tab.ts
+++ src/content/home-user-tab.ts
@@ -2,11 +2,11 @@ export const userContent = {
     title: 'A Path Just for You',
     description: ' Understanding where you stand is the first step to healing and helping. Let’s begin your journey with care that’s made for you.',
     users: [
-        { icon: '0', label: 'patient0', type: 'Patient'},
-        { icon: '1', label: 'patient1', type: 'Patient'},
-        { icon: '2', label: 'patient2', type: 'Patient'},
-        { icon: '3', label: 'patient3', type: 'Patient'},              
-        { icon: '4', label: 'patient4', type: 'Patient'},         
+        { icon: '0', label: 'Stage 0 Patient', type: 'Patient'},
+        { icon: '1', label: 'Stage 1 Patient', type: 'Patient'},
+        { icon: '2', label: 'Stage 2 Patient', type: 'Patient'},
+        { icon: '3', label: 'Stage 3 Patient', type: 'Patient'},              
+        { icon: '4', label: 'Stage 4 Patient', type: 'Patient'},         
     ],
     other: [
         { icon: 'caregiver', label: 'Caregiver', type:  'Caregiver' },
