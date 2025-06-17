664d5699d9f249265056fb318f6c82f7eabe4746
arsenie-sarmiento <cpt.iqhwa.vx7@gmail.com>
2025-06-18 05:07:47 +1200

    adjusted responsive vertical layout

--- src/content/home-resource.ts
+++ src/content/home-resource.ts
@@ -5,120 +5,120 @@ export const resourceContent = {
         'patient0': [
             {
             heading: 'Stage 0 Heading 1',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            cardLink: '/#'
+            content: 'Lsssorem ipsum dolor sit patient0, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            cardLink: '/patient0'
             },
             {
             heading: 'Stage 0 Heading 2',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            cardLink: '/#'
+            content: 'Lorem ipsum dolordsdsds sit patient0, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            cardLink: '/patient0'
             },
             {
             heading: 'Stage 0 Heading 3',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            cardLink: '/#'
+            content: 'Lorem ipsudsdsm dolor sit patient0, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            cardLink: '/patient0'
             }
         ],
         'patient1': [
             {
             heading: 'Stage 1 Heading 1',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            cardLink: '/#'
+            content: 'patient1 ipsum dolsdsdsor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            cardLink: '/patient1'
             },
             {
             heading: 'Stage 1 Heading 2',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            cardLink: '/#'
+            content: 'patient1 ipsumsfghh dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            cardLink: '/patient1'
             },
             {
             heading: 'Stage 1 Heading 3',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            cardLink: '/#'
+            content: 'patient1 ipsum dolor sit amfdfsscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            cardLink: '/patient1'
             }
         ],
         'patient2': [
             {
             heading: 'Stage 2 Heading 1',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            cardLink: '/#'
+            content: 'patient2 ipsum dolofsfr adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            cardLink: '/patient2'
             },
             {
             heading: 'Stage 2 Heading 2',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            cardLink: '/#'
+            content: 'patient2 ipfsfstur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            cardLink: '/patient2'
             },
             {
             heading: 'Stage 2 Heading 3',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            cardLink: '/#'
+            content: 'patient2 ipsumsfsfdipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            cardLink: '/patient2'
             }
         ],
         'patient3': [
             {
             heading: 'Stage 3 Heading 1',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            cardLink: '/#'
+            content: 'patient3 isffssectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            cardLink: '/patient3'
             },
             {
             heading: 'Stage 3 Heading 2',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            cardLink: '/#'
+            content: 'patient3 ipssfsfr adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            cardLink: '/patient3'
             },
             {
             heading: 'Stage 3 Heading 3',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            cardLink: '/#'
+            content: 'patient3 ipsufsfur adipiscing elit. Sed do eifffusmod tempor incididunt ut labore et dolore magna aliqua.',
+            cardLink: '/patient3'
             }
         ],
         'patient4': [
             {
             heading: 'Stage 4 Heading 1',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            cardLink: '/#'
+            content: 'patient4 ipsum dolor sit amet, consectetur adipfffiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            cardLink: '/patient4'
             },
             {
             heading: 'Stage 4 Heading 2',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            cardLink: '/#'
+            content: 'patient4 ipsum dolor sit amet, ffffconsectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            cardLink: '/patient4'
             },
             {
             heading: 'Stage 4 Heading 3',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            cardLink: '/#'
+            content: 'patient4 ipsum dolor sitffffamet, consectetufffr adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            cardLink: '/patient4'
             }
         ],
         'Caregiver': [
             {
             heading: 'Caregiver Heading 1',
-            content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            cardLink: '/#'
+            content: 'Lorem ipsum dolor sffffit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            cardLink: '/Caregiver'
             },
             {
             heading: 'Caregiver Heading 2',
             content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            cardLink: '/#'
+            cardLink: '/Caregiver'
             },
             {
             heading: 'Caregiver Heading 3',
             content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            cardLink: '/#'
+            cardLink: '/Caregiver'
             }
         ],
         'Researcher': [
             {
             heading: 'Researcher Heading 1',
             content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            cardLink: '/#'
+            cardLink: '/Researcher'
             },
             {
             heading: 'Researcher Heading 2',
             content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            cardLink: '/#'
+            cardLink: '/Researcher'
             },
             {
             heading: 'Researcher Heading 3',
             content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            cardLink: '/#'
+            cardLink: '/Researcher'
             }
         ]
     }
